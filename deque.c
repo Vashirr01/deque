1811128bd2b02c0d5f303cfd14293cafe8617ebe
vashirr <130561897+Vashirr01@users.noreply.github.com>
2024-12-16 12:52:19 +0530

    refactored struct defs to only one. defined the init function which returns a pointer to the deque

--- deque.c
+++ deque.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct nodes{
-	int key;
-	int value;
-} nodes;
-
 typedef struct deque{
-	nodes head;
-	nodes next;
+	int head;
+	int data;
+	struct deque *next;
 } deque;
 
 int main() {
 	printf("hello\n");
 }
 
-deque *deque_init(int value){
+deque *deque_init(int data){
+	deque *q = malloc(sizeof(deque));
 
+	q->head = 0;
+	q->data = data;
+	q->next = NULL;
+
+	return q;
 }
