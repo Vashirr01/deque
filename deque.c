7d1aef82d418e84f8b1532c809d1734085c971c6
vashirr <130561897+Vashirr01@users.noreply.github.com>
2024-12-16 12:33:54 +0530

    basic linked list and note structs currently single sided. also defined the init fn

--- deque.c
+++ deque.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodes{
+	int key;
+	int value;
+} nodes;
+
+typedef struct deque{
+	nodes head;
+	nodes next;
+} deque;
 
 int main() {
 	printf("hello\n");
+}
+
+deque *deque_init(int value){
+
 }
