3bffcc84b4880720131c9f5744f7544aaea098e1
vashirr <130561897+Vashirr01@users.noreply.github.com>
2024-12-16 13:49:32 +0530

    added a deque_add function which takes a value and the q and appends a node extending the list

--- deque.c
+++ deque.c
@@ -7,10 +7,6 @@ typedef struct deque{
 	struct deque *next;
 } deque;
 
-int main() {
-	printf("hello\n");
-}
-
 deque *deque_init(int data){
 	deque *q = malloc(sizeof(deque));
 
@@ -20,3 +16,31 @@ deque *deque_init(int data){
 
 	return q;
 }
+
+void deque_add(deque *q, int data){
+	int head = 1;
+	deque *node = malloc(sizeof(deque));
+	node->data = data;
+	node->next = NULL;
+
+	while (q->next != NULL) {
+		q = q->next;
+		head++;
+	}
+
+	q->next = node;
+	q->next->head = head;
+}
+
+int main() {
+	deque *q = deque_init(25);
+	deque_add(q, 69);
+	deque_add(q, 829);
+	deque_add(q, 687438);
+
+	do{
+	printf("head: %d, data: %d\n", q->head, q->data);
+		q = q->next;
+	}while (q != NULL) ;
+}
+
